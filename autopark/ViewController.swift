b27cec8a8493133d40a19031236cbdedb066b826
Ekaterina <sergievskaya_ea@mail.ru>
2024-10-06 14:09:22 +0700

    Initial Commit

---
+++ autopark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autopark
+//
+//  Created by Ekaterina on 06.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
